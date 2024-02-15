4ab151fb3a7cf5a35785bfab0c16ecb86d0e41f6
aniketsharma953 <sharmaniket593@gmail.com>
2024-02-15 16:51:41 +0530

    special

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-    function Message() {
-      alert("I love you!");
-    }
+function Message() {
+  alert("I love you!");
+}
+async function sharePage() {
+  await navigator.share({
+    title: document.title,
+    url: window.location.href,
+  });
+}
